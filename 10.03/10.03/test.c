ea17494b12bb805f73b6d16bcf8fd0376eab6f0b
林泽涛 <2925833459@qq.com>
2024-10-03 17:19:47 +0800

    变量初步认识

---
+++ 10.03/10.03/test.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+	int age = 20;
+	double weight = 60.3;
+
+	age = age + 1;
+	weight = weight - 10;
+
+	printf("%d\n",age);
+	printf("%lf\n",weight);
+
+	return 0;
+}
