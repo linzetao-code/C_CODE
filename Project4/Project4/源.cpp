770c7166df648cb6cbc26c6a37bd06b4e7ebed65
林泽涛 <2925833459@qq.com>
2024-10-10 18:56:12 +0800

    学习

---
+++ Project4/Project4/源.cpp
@@ -0,0 +1,24 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+int main() 
+{
+    int fahr, lower, upper;
+    double celsius;
+    scanf_s("%d", &lower);
+    printf("Enter lower=%d", lower);
+    scanf_s("%d", &upper);
+        
+    printf("Enter upper=%d", upper);
+    
+    
+    if (lower <= upper) {
+        printf("fahr celsius\n");
+        for (fahr = lower; fahr <= upper; fahr++) {
+            celsius = (5.0 / 9.0)*(fahr-32);
+            printf("%4d%6.lf",fahr, celsius);
+        }
+    }else{
+        printf("Invalid Value!\n");
+    }
+    return 0;
+    }
