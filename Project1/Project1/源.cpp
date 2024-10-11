f520b9ac529ff975deff6c12e22770de583eb110
林泽涛 <2925833459@qq.com>
2024-10-11 09:26:45 +0800

    另一个文件夹

--- Project1/Project1/源.cpp
+++ Project1/Project1/源.cpp
@@ -1,13 +1,8 @@
-#i#include<stdio.h>
 #include<stdio.h>
+
 int main()
 {
-    int a = 0;
-    int b = 0;
-    int sum = 0;
-    scanf_s("%d %d", &a, &b);
-    sum = a + b;
-    printf("sum=%d\n", sum);
+    printf("Hello,world!");
 
     return 0;
 }
