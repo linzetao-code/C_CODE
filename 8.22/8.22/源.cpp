6db506a648648481764bcce7f5d6ae676ba7e174
林泽涛 <2925833459@qq.com>
2024-10-11 23:20:19 +0800

    以前的

---
+++ 8.22/8.22/源.cpp
@@ -0,0 +1,13 @@
+#i#include<stdio.h>
+#include<stdio.h>
+int main()
+{
+    int a = 0;
+    int b = 0;
+    int sum = 0;
+    scanf_s("%d %d", &a, &b);
+    sum = a + b;
+    printf("sum=%d\n", sum);
+
+    return 0;
+}
